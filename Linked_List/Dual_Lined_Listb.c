c26a7c30b5a9fd68d7d8e649f8cb3678f7a9c47d
al-nusrati <jawadbinahmed06@gmail.com>
2026-01-16 00:36:18 +0500

    Linked_List b added

--- Linked_List/Dual_Lined_Listb.c
+++ Linked_List/Dual_Lined_Listb.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,9 +6,8 @@ struct node
     int data;
     //---
     struct node *next;
+    struct node *prev;
 };
-
-
 struct node *getNewNode(int val){
     // allocate mem for node
     struct node *newNode = (struct node*)malloc(sizeof(struct node));
@@ -17,6 +15,7 @@ struct node *getNewNode(int val){
     // fill node structure
     newNode->data = val;
     newNode->next = NULL;
+    newNode->prev = NULL;
 
     // return this new node
     return newNode;
@@ -44,7 +43,7 @@ int nodesCount(struct node *ptr)
     }
     return count;
 }
-void traverse(struct node *n1){
+void traverseForward(struct node *n1){
     struct node *newNode = n1;
 
     while(newNode != NULL){
@@ -54,15 +53,22 @@ void traverse(struct node *n1){
     printf(" NULL");
 }
 
+void traverseBackward(struct node *nodePtr){
+    struct node *currNode = nodePtr;
 
-
-
-
+    while(currNode != NULL){
+        printf("%d -> ", currNode->data);
+        currNode = currNode->prev;
+    }
+    printf(" NULL");
+}
 
 
 struct node *insertAtBeginning(struct node *ptr, int val){
     struct node *newNode = getNewNode(val);
     newNode->next = ptr;
+
+    ptr->prev = newNode;
     return newNode;
 }
 
@@ -82,8 +88,9 @@ struct node *insertAtEnd(struct node *nodePtr, int val){
     struct node *lastNode = getLastNode(nodePtr);
     struct node *newNode = getNewNode(val);
     
+    newNode->next = NULL;
+    newNode->prev = lastNode;
     lastNode->next = newNode;
-
     return newNode;
 }
 
@@ -113,38 +120,33 @@ int main(){
     n3->next = n4;
     n4->next = n5;
 
+    n2->prev = n1;
+    n3->prev = n2;
+    n4->prev = n3;
+    n5->prev = n4;
+
     // insert before --> b1 = 110, b2 = 120, b3 = 130
     struct node *b1 = insertAtBeginning(n1, 110);
     struct node *b2 = insertAtBeginning(b1, 120);
     struct node *b3 = insertAtBeginning(b2, 130);
 
-    // insert at end
     struct node *e1 = insertAtEnd(b3, 210);
     struct node *e2 = insertAtEnd(b3, 220);
     struct node *e3 = insertAtEnd(n4, 230);
-    
 
-    traverse(b3);    
+    traverseForward(b3);
+    printf("\n");
+    traverseBackward(e3);
 
-    int totalNodes = nodesCount(b3);            // 11
-    int dataLastNode = getLastNodeData(b3);     // 230
-
+    int totalNodes = nodesCount(n1);
     printf("\nTotal nodes = %d",totalNodes);
+
+    int dataLastNode = getLastNodeData(n1);
     printf("\nLast node val = %d", dataLastNode);
 
     freeMemory(b3);
 
-    // free(e1);
-    // free(e2);
-    // free(e3);
-    // free(n1);
-    // free(n2);
-    // free(n3);
-    // free(n4);
-    // free(n5);
-    // free(b1);
-    // free(b2);
-    // free(b3);
+    n1 = n2 = n3 = n4 = n5 = b1 = b2 = b3 = e1 = e2 = e3 = NULL;
 
-    return 0;
+    return 0;    
 }
